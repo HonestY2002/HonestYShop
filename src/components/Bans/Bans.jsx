b5882e0689138f2807f1bd21df53e81b5faf24e5
Andrey golishin <agolishin@inbox.ru>
2025-06-30 17:45:04 +0300

    table bans users

--- src/components/Bans/Bans.jsx
+++ src/components/Bans/Bans.jsx
@@ -1,9 +1,36 @@
+import { DataBans } from "./DataBans";
+
 function Bans() {
-    return(
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <h2>Заблокированные пользователим</h2>
+      <input
+        type="text"
+        placeholder="Поиск"
+      />
+
+      <table>
+        <thead>
+            <tr>
+                <th>Начало</th>
+                <th>Конец</th>
+                <th>Пользователь</th>
+                <th>Причина</th>
+            </tr>
+        </thead>
+        {DataBans.map((user, index) =>(
+            <tbody>
+                <tr key={index}>
+                    <td>{user.start}</td>
+                    <td>{user.end}</td>
+                    <td>{user.user}</td>
+                    <td>{user.reason}</td>
+                </tr>
+            </tbody>
+        ))}
+      </table>
+    </div>
+  );
 }
 
 export default Bans;
